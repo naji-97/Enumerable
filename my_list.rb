08c6c5a5ca5f7fc6ef7549cf2ca14173ead51dcc
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-15 13:58:59 +0000

    Add code to verify the solution

--- my_list.rb
+++ my_list.rb
@@ -11,3 +11,11 @@ class MyList
     @list.each { |item| yield item }
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts(list.all? {|e| e < 5})
+puts(list.all? {|e| e > 5})
+puts(list.any? {|e| e == 2})
+p(list.any? {|e| e == 5})
+p(list.filter {|e| e.even?})
